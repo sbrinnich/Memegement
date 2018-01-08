0bbb4e9e4675f2890add40fd34fe48e957633566
Gumi22 <gumi22-_-@hotmail.com>
2018-01-08 17:08:07 +0100

    added testfile for stupid temporary code

---
+++ sql/testfile.sql
@@ -0,0 +1,7 @@
+Use Memegement;
+GO
+
+SELECT * FROM Gruppe;
+
+INSERT INTO Gruppe (name, beschreibung, gruendungsDatum, gruenderId, gruppenBild) VALUES
+  ('Testy', '', '2017-12-12', 1,1);
--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,13 +1,32 @@
 use Memegement
 GO
 
-DROP TRIGGER IF EXISTS tr_newRating;
+DROP TRIGGER IF EXISTS tr_neueBewertung;
 GO
 
-CREATE TRIGGER  tr_newRating
-  ON Memegement.dbo.Bewertung
+CREATE TRIGGER  tr_neueBewertung
+  ON Bewertung
   FOR INSERT
 AS
-  DECLARE @BewertungsObjekt int = (Select bewertungsObjekt FROM inserted)
-UPDATE Memegement.dbo.FunObjekt SET durchschnittsBewertung = (SELECT avg(bewertung) FROM Bewertung WHERE bewertungsObjekt = @BewertungsObjekt)
+  DECLARE @BewertungsObjekt int = (Select bewertungsObjekt FROM inserted);
+UPDATE FunObjekt SET durchschnittsBewertung = (SELECT avg(bewertung) FROM Bewertung WHERE bewertungsObjekt = @BewertungsObjekt)
 WHERE id = @BewertungsObjekt;
+
+
+DROP TRIGGER IF EXISTS tr_neueGruppe;
+GO
+
+CREATE TRIGGER tr_neueGruppe ON Gruppe
+AFTER INSERT
+AS
+  DECLARE @gruppenId int, @erstellerId int, @inCount int;
+
+  SET @inCount = (Select Count(*) FROM inserted);
+  IF(@inCount > 0)
+      BEGIN
+        SET @gruppenId = (Select id FROM inserted);
+        SET @erstellerId = (Select gruenderId FROM inserted);
+        INSERT INTO GruppenMitgliedschaft (trollId, gruppenId, beitrittsDatum) VALUES
+          (@erstellerID, @gruppenId, GETDATE());
+      END
+GO
