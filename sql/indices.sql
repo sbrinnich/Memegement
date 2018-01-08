0b00b0c10a1da0536d8aa3ef8d8ea7264d1d2c6b
Alexander Dietrich <if16b063@technikum-wien.at>
2018-01-08 17:02:00 +0100

    insert lot of data

--- sql/indices.sql
+++ sql/indices.sql
@@ -3,3 +3,5 @@ create index ix_trollnamen on Troll (benutzerName);
 create index ix_funobjektTitel on FunObjekt (titel);
 
 create index ix_gruppennamen on Gruppe (name);
+
+CREATE INDEX ix_kommentarObjekt on Kommentar (kommentarObjekt);
