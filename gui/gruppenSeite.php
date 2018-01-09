a8935b6f1cd5b6b57195f0f5c3528168274ac894
Alexander Dietrich <if16b063@technikum-wien.at>
2018-01-09 23:44:38 +0100

    Gruppenseite fertig

--- gui/gruppenSeite.php
+++ gui/gruppenSeite.php
@@ -12,6 +12,8 @@ if(isset($_GET["groupID"])){
     $currentGroupID = $_GET["groupID"];
 }
 
+
+
 $name = '';
 $beschreibung = '';
 $gruendungsDatum = '';
@@ -38,25 +40,75 @@ $conn = sqlsrv_connect( $DB_HOST, $connectionInfo);
 $sql = "EXEC usp_gruppenDatenAnzeigen @id = ? , @name = ? , @beschreibung = ? , @gruendungsDatum = ? , @gruenderName = ? , @mitgliederAnzahl = ? , @gruppenBildLink = ?";
 $stmt = sqlsrv_prepare($conn, $sql, $groupInformation);
 
-echo "test 0";
-
 if(sqlsrv_execute($stmt)) {
-    echo "test 1";
     sqlsrv_next_result($stmt);
-
-    echo "TEST 2";
 
     ?>
 
-    <img src="<?php echo $gruppenBildLink ?>">
+    <img class="img-group" src="<?php echo $gruppenBildLink ?>">
+    <div class="div-group-site">
+        <h1 class="group-name-xxl"><?php echo $name ?></h1><br/><br/>
+        <h2 class="group-creator">Gegründet von: <?php echo $gruenderName ?></h2><br/><br/>
+        <h3 class="group-date">am: <?php echo $gruendungsDatum ?></h3><br/><br/>
+        <h3 class="group-members"><?php echo $mitgliederAnzahl ?> Mitglieder</h3><br/><br/><br/><br/>
+        <form method="post" action=#>
+            <input value="Gruppe beitreten" class="btn btn-primary" type="submit" name="join_group"/>
+        </form>
+    </div>
 
 
     <?php
-
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);
 }else{
-    echo "Error Executing";
     echo print_r(sqlsrv_errors(), true);
 }
 
+if(isset($_POST["join_group"])) {
+    $CurrentUserID = 0;
+
+    $parameterGetID = array(
+        array($_SESSION['loginUsername'] , SQLSRV_PARAM_IN),
+        array(&$CurrentUserID , SQLSRV_PARAM_INOUT)
+    );
+
+    $connectionInfo = array( "UID"=>$DB_USERNAME,
+        "PWD"=>$DB_PASSWORD,
+        "Database"=>$DB_NAME);
+
+    $conn = sqlsrv_connect( $DB_HOST, $connectionInfo);
+
+    $sql = "EXEC usp_benutzerIdSuchen @benutzerName = ? , @id = ?";
+    $stmt = sqlsrv_prepare($conn, $sql, $parameterGetID);
+
+    if(sqlsrv_execute($stmt)){
+        sqlsrv_next_result($stmt);
+
+        if($CurrentUserID != 0){
+            $joinGroupParameter = array(
+                    array($CurrentUserID, SQLSRV_PARAM_IN),
+                    array($currentGroupID, SQLSRV_PARAM_IN)
+            );
+            $sql = "EXEC usp_gruppeBeitreten @trollId = ? , @gruppenId = ?";
+            $stmt = sqlsrv_prepare($conn, $sql, $joinGroupParameter);
+
+            if(sqlsrv_execute($stmt)){
+                sqlsrv_next_result($stmt);
+
+            }else{
+                echo print_r(sqlsrv_errors(), true);
+            }
+
+        }else{
+            echo "No User-ID found!";
+        }
+    }else{
+        echo print_r(sqlsrv_errors(), true);
+    }
+
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);
+
+}
 
 ?>
