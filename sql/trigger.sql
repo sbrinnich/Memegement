292a709adc1580ab00e695866b30ac2e7f4107cb
Gumi22 <gumi22-_-@hotmail.com>
2018-01-09 11:27:57 +0100

    Trigger fertig denk ich

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,12 +1,14 @@
 USE Memegement
 GO
 
+-- Bei einer neuen Bewertung muss die Durchschnittsbewertung des FunObjekts aktualisiert werden.
+
 DROP TRIGGER IF EXISTS tr_neueBewertung;
 GO
 
 CREATE TRIGGER  tr_neueBewertung
   ON Bewertung
-  FOR INSERT
+  AFTER INSERT
 AS
   DECLARE @bewertungsObjekt int = (Select bewertungsObjekt FROM inserted),
           @durchschnittsBewertung float = (SELECT avg(bewertung) FROM Bewertung WHERE bewertungsObjekt = @BewertungsObjekt);
@@ -15,6 +17,7 @@ AS
       WHERE id = @bewertungsObjekt;
 GO
 
+-- Bei einer neuen Gruppe soll der Gründer automatisch als Gruppenmitglied hinzugefügt werden.
 
 DROP TRIGGER IF EXISTS tr_neueGruppe;
 GO
@@ -32,6 +35,65 @@ AS
         INSERT INTO GruppenMitgliedschaft (trollId, gruppenId, beitrittsDatum) VALUES
           (@erstellerID, @gruppenId, GETDATE());
       END
+GO
+
+-- Bei einem neuen User soll geprüft werden ob der Name gültig und der Hash lang genug sind
+
+DROP TRIGGER IF EXISTS tr_neuerUser;
+GO
+
+CREATE TRIGGER tr_neuerUser
+  ON Troll
+AFTER INSERT
+AS
+  DECLARE @benutzerName VARCHAR(15) = (Select benutzerName FROM inserted),
+  @passwortHash VARCHAR(256) = (Select passwortHash FROM inserted);
+
+  IF(ltrim(rtrim(isNull(@benutzerName,''))) = '' OR @passwortHash < 64)
+      BEGIN
+        RAISERROR ('Benutzername oder Passwort zu kurz', 10,1);
+        ROLLBACK
+      END
+
+GO
+
+-- Bei einem neuen FunObjekt soll geprüft werden ob der titel gültig und die Durchschnittsbewertung NULL sind
+
+DROP TRIGGER IF EXISTS tr_neuesFunObjekt;
+GO
+
+CREATE TRIGGER tr_neuesFunObjekt
+  ON FunObjekt
+AFTER INSERT
+AS
+  DECLARE @titel VARCHAR(50) = (Select titel FROM inserted),
+  @bewertung FLOAT = (Select durchschnittsBewertung FROM inserted);
+
+  IF(ltrim(rtrim(isNull(@titel,''))) = '' OR @bewertung IS NOT NULL)
+    BEGIN
+      RAISERROR ('titel zu kurz oder Bewertung nicht NULL', 10,1);
+      ROLLBACK
+    END
+
+GO
+
+-- Bei einem neuen Kommentar soll geprüft werden ob der text gültig ist
+
+DROP TRIGGER IF EXISTS tr_neuesKommentar;
+GO
+
+CREATE TRIGGER tr_neuesKommentar
+  ON Kommentar
+AFTER INSERT
+AS
+  DECLARE @text VARCHAR(50) = (Select text FROM inserted);
+
+  IF(ltrim(rtrim(isNull(@text,''))) = '')
+    BEGIN
+      RAISERROR ('Text ungültig', 10,1);
+      ROLLBACK
+    END
+
 GO
 
 
