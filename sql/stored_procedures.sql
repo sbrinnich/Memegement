3d39b301640c176247cb09f5b5da1626d6806e33
Phillip <phillip-schermann@hotmail.com>
2018-01-09 14:08:02 +0100

    trigger

--- sql/stored_procedures.sql
+++ sql/stored_procedures.sql
@@ -8,7 +8,7 @@ AS
 BEGIN TRY
 BEGIN TRANSACTION
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED
-INSERT INTO dbo.Troll(benutzerName,passwortHash,beitrittsDatum,profilBild) values
+INSERT INTO dbo.Troll(benutzerName,passwortHash,beitrittsDatum) values
   ( @benutzerName,@passwortHash, GETDATE())
 COMMIT TRANSACTION
 END TRY
