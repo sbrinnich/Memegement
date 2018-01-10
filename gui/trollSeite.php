a61e1affd7fedd2104ebc5b9473eb3fb805e123d
Selina Brinnich <if16b030@technikum-wien.at>
2018-01-10 09:16:52 +0100

    Added troll page

--- gui/trollSeite.php
+++ gui/trollSeite.php
@@ -1,33 +1,15 @@
 <?php
 
+if(!isset($_GET['id']) || $_GET['id'] == ''){
+    header('Location: index.php?seite=home', true, 301);
+    exit();
+}
+
+$id = $_GET['id'];
+
 $connectionInfo = array( "UID"=>$DB_USERNAME,
     "PWD"=>$DB_PASSWORD,
     "Database"=>$DB_NAME);
-
-$conn = sqlsrv_connect( $DB_HOST, $connectionInfo);
-
-$benutzerName = $_SESSION['loginUsername'];
-$id = 0;
-
-$procedure_params = array(
-    array($benutzerName, SQLSRV_PARAM_IN),
-    array(&$id, SQLSRV_PARAM_INOUT),
-);
-
-$sql = "EXEC usp_benutzerIdSuchen @benutzerName = ?, @id = ? ";
-$stmt = sqlsrv_prepare($conn, $sql, $procedure_params);
-
-if(sqlsrv_execute($stmt)) {
-    sqlsrv_next_result($stmt);
-
-    sqlsrv_free_stmt($stmt);
-
-
-    sqlsrv_close($conn);
-}else{
-    $_SESSION['status'] = 'Ein Fehler ist aufgetreten! Benutzer konnte nicht erstellt werden!';
-    sqlsrv_close($conn);
-}
 
 $conn = sqlsrv_connect( $DB_HOST, $connectionInfo);
 
@@ -44,11 +26,12 @@ if(sqlsrv_execute($stmt)) {
             ?>
 
             <div class="row">
-                <div class="col-md-4"></div>
                 <div class="col-md-4">
-                    <?php
-                    echo '<img class="img-group" src="'.$row['link'].'" /><h1>'.$row['benutzerName'].'</h1><h3>'.$row['beitrittsDatum']->format('Y-m-d').'</h3>';
-                    ?>
+                    <img class="img-group" src="<?php echo $row['link']; ?>" />
+                </div>
+                <div class="col-md-4">
+                    <h1><?php echo $row['benutzerName']; ?></h1>
+                    <h3>Beigetreten am <?php echo $row['beitrittsDatum']->format('Y-m-d'); ?></h3>
                 </div>
             </div>
 
@@ -64,84 +47,80 @@ if(sqlsrv_execute($stmt)) {
     sqlsrv_close($conn);
 }
 
-?>
-
-<?php
 $conn = sqlsrv_connect( $DB_HOST, $connectionInfo);
 
 $procedure_params = array(
-array($id, SQLSRV_PARAM_IN)
+    array($id, SQLSRV_PARAM_IN),
+    array(0, SQLSRV_PARAM_IN),
+    array(10, SQLSRV_PARAM_IN)
 );
 
 $sql = "EXEC usp_benutzerWitzeAnzeigenNachBewertung @id = ?,@offset = ? ,@limit = ?";
 $stmt = sqlsrv_prepare($conn, $sql, $procedure_params);
 
 if(sqlsrv_execute($stmt)) {
-do{
-while($row = sqlsrv_fetch_array($stmt)){
-?>
+    do{
+        while($row = sqlsrv_fetch_array($stmt)){
+        ?>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-3">
+                        <div class="panel panel-default index_panel">
 
-
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-3">
-                <div class="panel panel-default index_panel">
-
-                    <div class="panel-body index_panelbody index_panelleft">
-                        Neueste Beiträge
-                    </div>
-                    <div class="panel-body index_panelbody index_panelleft">
-                        <div class="col-md-3">
-                            <p style="color: white">Video</p>
+                            <div class="panel-body index_panelbody index_panelleft">
+                                Neueste Beiträge
+                            </div>
+                            <div class="panel-body index_panelbody index_panelleft">
+                                <div class="col-md-3">
+                                    <p style="color: white">Video</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <p style="color: white">Bild</p>
+                                </div>
+                                <div class="col-md-3">
+                                    <p style="color: white">Witz</p>
+                                </div>
+                            </div>
+                            <div class="panel-body index_panelbody index_panelleft">
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <p style="color: white">Bild</p>
-                        </div>
-                        <div class="col-md-3">
-                            <p style="color: white">Witz</p>
-                        </div>
-                    </div>
-                    <div class="panel-body index_panelbody index_panelleft">
-                    </div>
                 </div>
-        </div>
 
-        <div class="col-md-3">
-            <div class="panel panel-default index_panel">
-                <div class="panel-body index_panelbody index_panelright">
-                    Beste Bewertungen
-                </div>
-                <div class="panel-body index_panelbody index_panelleft">
-                    <div class="col-md-3">
-                        <p style="color: white">Video</p>
-                    </div>
-                    <div class="col-md-3">
-                        <p style="color: white">Bild</p>
-                    </div>
-                    <div class="col-md-3">
-                        <p style="color: white">Witz</p>
-                        <?php
-                        echo '<h1>'.$row['text'].'</h1>';
-                        ?>
-                    </div>
-                </div>
-                <div class="panel-body index_panelbody index_panelleft">
+                <div class="col-md-3">
+                    <div class="panel panel-default index_panel">
+                        <div class="panel-body index_panelbody index_panelright">
+                            Beste Bewertungen
+                        </div>
+                        <div class="panel-body index_panelbody index_panelleft">
+                            <div class="col-md-3">
+                                <p style="color: white">Video</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p style="color: white">Bild</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p style="color: white">Witz</p>
+                                <?php
+                                echo '<h1>'.$row['text'].'</h1>';
+                                ?>
+                            </div>
+                        </div>
+                        <div class="panel-body index_panelbody index_panelleft">
 
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
 
-    <?php
-}
-}while(sqlsrv_next_result($stmt));
+            <?php
+        }
+    }while(sqlsrv_next_result($stmt));
     sqlsrv_free_stmt($stmt);
 
-
-    sqlsrv_close($conn);
 }else{
-    $_SESSION['status'] = 'Ein Fehler ist aufgetreten! Benutzer konnte nicht erstellt werden!';
-    sqlsrv_close($conn);
+    echo print_r(sqlsrv_errors(), true);
+    $_SESSION['status'] = 'Ein Fehler ist aufgetreten! Benutzer konnte nicht geladen werden!';
 }
+sqlsrv_close($conn);
 ?>
